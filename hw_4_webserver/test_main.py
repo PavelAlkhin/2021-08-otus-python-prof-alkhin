ee2360caa654415418aaabbb2f6e309732551200
PavelAlkhin <unicon2000@gmail.com>
2021-10-11 11:13:11 +0300

    1

---
+++ hw_4_webserver/test_main.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+
+import requests
+
+from hw_4_webserver.httpd import parse_request_line
+
+HEADER = {'Content-Type': 'text/html'}
+TEXT_REQUEST = b'GET /sa/index.html HTTP/1.1\r\nHost: 127.0.0.1:9999'
+
+
+class Test(TestCase):
+    def test_main(self):
+        answer = requests.get(url='http://127.0.0.1:9999')
+        self.assertEqual(answer.headers, HEADER)
+
+    def test_parse_request_line(self):
+        data = parse_request_line(TEXT_REQUEST.decode())
+        method = data['method']
+        resource = data['resource']
+        self.assertEqual(method, 'GET')
+        self.assertEqual(resource, '/sa/index.html')
--- hw_5/test_main.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-
-import requests
-
-
-class Test(TestCase):
-    def test_main(self):
-        requests.get(url='http://127.0.0.1:9999')
